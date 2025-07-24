40a34d850b778e3cf6c9eeb61a807b30dbb5d47b
elitekaycy <dicksonanyaele1234@gmail.com>
2025-07-24 19:10:47 +0000

    fix url

---
+++ src/services/api/api-const.ts
@@ -0,0 +1,8 @@
+// src/constants/env.ts
+
+export const ENV = {
+    API_URL: import.meta.env.VITE_API_URL,
+    APP_NAME: import.meta.env.VITE_APP_NAME,
+    NODE_ENV: import.meta.env.MODE, 
+  };
+  
